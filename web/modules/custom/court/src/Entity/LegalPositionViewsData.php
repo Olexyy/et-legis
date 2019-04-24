d727cf6b7b3b153ad4848839335d81b7c24448e3
Olexyy <olexyy.mails@gmail.com>
2019-04-24 23:37:23 +0300

    Add relationship.

--- web/modules/custom/court/src/Entity/LegalPositionViewsData.php
+++ web/modules/custom/court/src/Entity/LegalPositionViewsData.php
@@ -13,10 +13,28 @@ class LegalPositionViewsData extends EntityViewsData {
    * {@inheritdoc}
    */
   public function getViewsData() {
-    $data = parent::getViewsData();
 
-    // Additional information for Views integration, such as table joins, can be
-    // put here.
+    $data = parent::getViewsData();
+    $data['taxonomy_term_field_data']['reverse__legal_position__tags']['relationship'] = [
+      'title' => t('Legal positions'),
+      'label' => t('Legal positions'),
+      'group' => t('Legal position'),
+      'help' => t('Reference to legal positions'),
+      'id' => 'entity_reverse',
+      'entity_type' => 'legal_position',
+      'base' => 'legal_position_field_data',
+      'base field' => 'id',
+      'field_name' => 'tags',
+      'field table' => 'legal_position__tags',
+      'field field' => 'tags_target_id',
+      'join_extra' => [
+        [
+          'field' => 'deleted',
+          'value' => 0,
+          'numeric' => TRUE,
+        ],
+      ],
+    ];
 
     return $data;
   }
